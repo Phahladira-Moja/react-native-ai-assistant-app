dd538313af955885b94c92745a67ed4376e234c1
Phahladira-Moja <phahladira@patala.co.za>
2023-07-09 03:51:49 +0200

    Added the api calls for open ai

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
-import { SafeAreaView, Text, View } from "react-native";
 import AppNavigation from "./navigation";
+import { useEffect } from "react";
+import { apiCall } from "./api/openAI";
 
 export default function App() {
+  useEffect(() => {
+    // apiCall("create an image of a dog");
+  }, []);
+
   return <AppNavigation />;
 }
